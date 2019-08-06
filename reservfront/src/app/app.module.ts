61d517a7f57e54074f7feb6af17c52d2b15322fd
Daniel L Cobos Prada <daniel.leocobos@gmail.com>
2019-08-06 14:06:54 -0500

    angular

--- reservfront/src/app/app.module.ts
+++ reservfront/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestauranteComponent } from './restaurante/restaurante.component';
+import { BarComponent } from './bar/bar.component';
+import { ReservaComponent } from './Bar/reserva/reserva.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestauranteComponent,
+    BarComponent,
+    ReservaComponent
   ],
   imports: [
     BrowserModule,
